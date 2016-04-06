1ed666a308174dcad166eab255fc627fa2715b25
King Li <kjli@codeaurora.org>
2016-04-06 18:34:18 +0800

    Wifi: Clear scan caches while loading configured networks
    Framework stores the scan caches for each configured network in a hash
    map with network id as key and never clear them. If removing a network
    and then re-enabling wifi, framework will re-assigned network id for
    each configured network while loading them, however, the scan caches
    still keeps the obsolete network id as its key.
    
    This change is to clear the scan caches when framework loads configured
    networks, which makes sure the network id in the scan caches is up to
    date.
    
    CRs-Fixed: 999659
    Change-Id: I1a6cdb2871e6f58f7aa5d233517d5983be91a7df

--- service/java/com/android/server/wifi/WifiConfigStore.java
+++ service/java/com/android/server/wifi/WifiConfigStore.java
@@ -1913,6 +1913,7 @@ public class WifiConfigStore extends IpConfigStore {
         mLastPriority = 0;
 
         mConfiguredNetworks.clear();
+        mScanDetailCaches.clear();
         List<WifiConfiguration> configTlsResetList = new ArrayList<WifiConfiguration>();
         int last_id = -1;
         boolean done = false;
